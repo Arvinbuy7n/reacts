45fce5415d1595171d817261c7f77379b8744dd8
Arvinbuyan Otgonjargal <23LP6614@G301-CC0182.local>
2023-11-15 13:10:24 +0800

    hell

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,103 @@ import styles from './page.module.css'
 import { useState } from 'react';
 
 export default function Home() {
-  const [count, setCount] = useState(4);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "To Do",
+      description: "+  Add card",
+    },
+    {
+      id: 2,
+      title: "In progress",
+      description: "+  Add card",
+    },
+    {
+      id: 3,
+      title: "Stuck",
+      description: "+  Add card",
+    },
+    {
+      id: 4,
+      title: "Done",
+      description: "+  Add card",
+    },
+  ]);
+  const clickHandler = () => {
+    setData([
+      ...data,
+      {
+        id: 4,
+        title: "Done",
+        description: "+  Add card",
+      },
+    ]);
+  };
 
   return (
-    <div className={styles.container} >
-      <Counter
-        count={count}
-        updateCount={() => {
-          setCount(count + 1)
-        }}
-      />
+    <>
+      <div className={styles.board}>
+        <div className={styles.container} >
+          {data.map((item) => {
+            return <Card key={item.id} title={item.title} description={item.description} />
+          })}
+          <button onClick={clickHandler}>Add</button>
+        </div>
+      </div>
+      <Name />
+    </>
+  );
+};
+
+export const Card = (props) => {
+  return (
+    <div className={styles.card} >
+      <h3 className={styles.num}>{props.title}</h3>
+      <p className={styles.num2}>{props.description}</p>
     </div>
   );
 };
-export const Counter = (props) => {
+
+export const Name = (count) => {
   return (
-    <div className={styles.number} >
-      <h3>{props.count}</h3>
-      <button className={styles.color} onClick={props.updateCount}>Add</button>
+    <div className={styles.core}>
+      <form className={styles.repo}>
+        <h2>Add task</h2>
+        <p className={styles.share}>Title</p>
+        <input className={styles.one}></input>
+        <p className={styles.share}>Description</p>
+        <textarea className={styles.onex}></textarea>
+        <p className={styles.share}>Status</p>
+        <select className={styles.one}>
+          <option></option>
+          <option>To do</option>
+          <option>In progress</option>
+          <option>Stuck</option>
+          <option>Done</option>
+        </select>
+        <p className={styles.share}>Priority</p>
+        <select className={styles.one}>
+          <option></option>
+          <option>High</option>
+          <option>Medium</option>
+          <option>Low</option>
+        </select>
+        <div className={styles.data}>
+          <button className={styles.too}>Add task</button>
+        </div>
+      </form>
     </div>
   );
 };
 
+
+
+
+
+
+
+{/* <div className={styles.card} >
+      <h3 className={styles.num}>{props.title}</h3>
+      <p className={styles.num2}>{props.description}</p>
+      <p className={styles.num3}>{props.priority}</p>
+    </div> */}
